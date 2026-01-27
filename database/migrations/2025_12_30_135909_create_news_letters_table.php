17eca26126721216f9d0d3291abacd5d48eaa4e7
sanjay-kr-mandal <sanjay.mandal@markupmail.com>
2026-01-27 11:53:15 +0530

    changes work

--- database/migrations/2025_12_30_135909_create_news_letters_table.php
+++ database/migrations/2025_12_30_135909_create_news_letters_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('news_letters', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->nullable(); 
             $table->string('email')->nullable(); 
             $table->timestamps();
         });
